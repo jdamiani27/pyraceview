362791510f1d8d799e5741efb2c08670351ada68
Jason Damiani <me@jasondamiani.com>
2018-03-24 01:58:04 -0400

    Use meaningful variable names in MsgCarPosition

--- pyraceview/messages/MsgCarPosition.py
+++ pyraceview/messages/MsgCarPosition.py
@@ -7,36 +7,32 @@ class MsgCarPosition(object):
     CAR_POSITION_CAR_BITS = uint32(8)
     CAR_POSITION_VITC_TIME_BITS = uint32(32)
 
-    def __init__(self, param1, param2):
-        _loc3_ = None
-        _loc4_ = 0
-        _loc5_ = None
+    def __init__(self, msg_header, byte_array):
         self._car_data = [] # PerCarPositionData
         self._car_reverse_lookup = {}
 
-        if param2:
-            _loc3_ = BitBuffer(param2)
-            _loc3_.set_position(7)
-            self._number_of_cars = _loc3_.get_bits(self.CAR_POSITION_CAR_BITS)
-            self._vitcTime = _loc3_.get_bits(self.CAR_POSITION_VITC_TIME_BITS)
-            _loc4_ = 0
+        bit_buffer = BitBuffer(byte_array)
+        bit_buffer.set_position(7)
+        self._number_of_cars = bit_buffer.get_bits(self.CAR_POSITION_CAR_BITS)
+        self._vitc_time = bit_buffer.get_bits(self.CAR_POSITION_VITC_TIME_BITS)
+        i = 0
 
-            while _loc4_ < self._number_of_cars:
-                _loc5_ = PerCarPositionData(_loc3_)
-                self._car_data.append(_loc5_)
-                self._car_reverse_lookup[_loc5_.car_id] = _loc5_
-                _loc4_ += 1
+        while i < self._number_of_cars:
+            position = PerCarPositionData(bit_buffer)
+            self._car_data.append(position)
+            self._car_reverse_lookup[position.car_id] = position
+            i += 1
 
     @property
     def car_data(self):
         return self._car_data
 
-    def get_car_by_id(self, param1):
-        return self._car_reverse_lookup[param1]
+    def get_car_by_id(self, car_id):
+        return self._car_reverse_lookup[car_id]
 
     @property
     def vitcTime(self):
-        return self._vitcTime
+        return self._vitc_time
 
     @property
     def number_of_cars(self):
