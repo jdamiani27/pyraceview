b1dcb17e676f0078f9c86e0f6ebbf4912eb521d6
Jason Damiani <me@jasondamiani.com>
2018-03-24 02:05:06 -0400

    Use meaningful variable names in MsgCupInfo

--- pyraceview/messages/MsgCupInfo.py
+++ pyraceview/messages/MsgCupInfo.py
@@ -8,17 +8,17 @@ class MsgCupInfo(object):
     CUP_POINTS_NUM_CAR_BITS = uint32(6)
 
     def __init__(self, msg_header, byte_array):
-        self._per_car_points = []
-        _loc3_ = BitBuffer(byte_array)
-        _loc3_.set_position(7)
-        self._lap = _loc3_.get_bits(self.CUP_POINTS_LAP_BITS)
-        self._num_cars = _loc3_.get_bits(self.CUP_POINTS_NUM_CAR_BITS)
+        self._per_car_points = [] # PerCarPointsData
+        bit_buffer = BitBuffer(byte_array)
+        bit_buffer.set_position(7)
+        self._lap = bit_buffer.get_bits(self.CUP_POINTS_LAP_BITS)
+        self._num_cars = bit_buffer.get_bits(self.CUP_POINTS_NUM_CAR_BITS)
 
-        _loc4_ = 0
+        i = 0
 
-        while _loc4_ < self._num_cars:
-            self._per_car_points.append(PerCarPointsData(_loc3_))
-            _loc4_ += 1
+        while i < self._num_cars:
+            self._per_car_points.append(PerCarPointsData(bit_buffer))
+            i += 1
 
     @property
     def per_car_points(self):
