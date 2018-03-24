571ff6714d6fed563834a5a370b18ca30593b20e
Jason Damiani <me@jasondamiani.com>
2018-03-24 00:55:29 -0400

    Remove commented actionscript code

--- pyraceview/messages/MsgTrackConfig.py
+++ pyraceview/messages/MsgTrackConfig.py
@@ -2,14 +2,13 @@ from numpy import uint32
 from ..util import BitBuffer
 
 
-class MsgTrackConfig(object):  # extends MsgBase
+class MsgTrackConfig(object):
     TRACKCONFIG_BITS_ORIGIN_X = uint32(32)
     TRACKCONFIG_BITS_ORIGIN_Y = uint32(32)
     TRACKCONFIG_BITS_ORIGIN_Z = uint32(32)
     TRACKCONFIG_BITS_TRACK_NAME = uint32(64)
 
     def __init__(self, msg_header, byte_array):
-        # super(param1)
         _loc3_ = BitBuffer(byte_array)
         _loc3_.set_position(7)
         self._local_origin_x = _loc3_.get_bits(self.TRACKCONFIG_BITS_ORIGIN_X)
