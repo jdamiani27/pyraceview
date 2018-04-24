2dedc29ba92622e5a678209902111880d9fe26bc
Jason Damiani <me@jasondamiani.com>
2018-04-23 20:20:57 -0400

    Clean up variable names

--- pyraceview/messages/MsgVitcToLap.py
+++ pyraceview/messages/MsgVitcToLap.py
@@ -12,17 +12,19 @@ class MsgVitcToLap(object):
     def __init__(self, msg_header, byte_array):
         self._vitc_indices = []
         self._flags = []
-        _loc3_ = BitBuffer(byte_array)
-        _loc3_.set_position(7)
-        self._start_idx = _loc3_.get_bits(self.VITCLAP_START_LAP_BITS)
-        self._num_lap_entries = _loc3_.get_bits(self.VITCLAP_NUM_ENTRIES_BITS)
-        _loc3_.get_bits(self.VITCLAP_RESERVED)
-        _loc4_ = 0
 
-        while _loc4_ < self._num_lap_entries:
-            self._vitc_indices.append(_loc3_.get_bits(self.VITCLAP_VITC_BITS))
-            self._flags.append(_loc3_.get_bits(self.VITCLAP_FLAG_BITS))
-            _loc4_ += 1
+        bit_buffer = BitBuffer(byte_array)
+        bit_buffer.set_position(7)
+        self._start_idx = bit_buffer.get_bits(self.VITCLAP_START_LAP_BITS)
+        self._num_lap_entries = bit_buffer.get_bits(self.VITCLAP_NUM_ENTRIES_BITS)
+        bit_buffer.get_bits(self.VITCLAP_RESERVED)
+
+        i = 0
+
+        while i < self._num_lap_entries:
+            self._vitc_indices.append(bit_buffer.get_bits(self.VITCLAP_VITC_BITS))
+            self._flags.append(bit_buffer.get_bits(self.VITCLAP_FLAG_BITS))
+            i += 1
 
     @property
     def flags(self):
