c25265110c0213143a51977f4c32af3de4070231
Jason Damiani <me@jasondamiani.com>
2018-04-23 20:19:57 -0400

    Clean up variable names

--- pyraceview/messages/MsgRaceStatus.py
+++ pyraceview/messages/MsgRaceStatus.py
@@ -21,23 +21,24 @@ class MsgRaceStatus(object):  # extends MsgBase
                                                  + BITS_SUNSET
                                                  + BITS_RESERVED) // 8)
 
-    def __init__(self, header, message):
+    def __init__(self, msg_header, byte_array):
         self._per_car_race_status = []  # PerCarRaceStatusData
-        _loc3_ = BitBuffer(message)
-        _loc3_.set_position(7)
-        self._vitc_time = float64(_loc3_.get_bits(self.BITS_VITC_TIME))
-        self._lap = _loc3_.get_bits(self.BITS_LAP)
-        self._flag = _loc3_.get_bits(self.BITS_FLAG)
-        self._number_cautions = _loc3_.get_bits(self.BITS_CAUTIONS)
-        self._last_flag_change_lap = _loc3_.get_bits(self.BITS_FLAG_LAP)
-        self._number_of_cars = _loc3_.get_bits(self.BITS_CARS)
-        self._sun_set_value = _loc3_.get_bits(self.BITS_SUNSET)
-        _loc3_.get_bits(self.BITS_RESERVED)
-        _loc4_ = uint32((header.size - self.PREAMBLE_SIZE_BYTES) // self._number_of_cars)
-        _loc5_ = 0
-        while _loc5_ < self._number_of_cars:
-            self._per_car_race_status.append(PerCarRaceStatusData(_loc3_, _loc4_))
-            _loc5_ += 1
+        bit_buffer = BitBuffer(byte_array)
+        bit_buffer.set_position(7)
+        self._vitc_time = bit_buffer.get_bits(self.BITS_VITC_TIME)
+        self._lap = bit_buffer.get_bits(self.BITS_LAP)
+        self._flag = bit_buffer.get_bits(self.BITS_FLAG)
+        self._number_cautions = bit_buffer.get_bits(self.BITS_CAUTIONS)
+        self._last_flag_change_lap = bit_buffer.get_bits(self.BITS_FLAG_LAP)
+        self._number_of_cars = bit_buffer.get_bits(self.BITS_CARS)
+        self._sun_set_value = bit_buffer.get_bits(self.BITS_SUNSET)
+        bit_buffer.get_bits(self.BITS_RESERVED)
+        byte_size = uint32((msg_header.size - self.PREAMBLE_SIZE_BYTES) // self._number_of_cars)
+
+        i = 0
+        while i < self._number_of_cars:
+            self._per_car_race_status.append(PerCarRaceStatusData(bit_buffer, byte_size))
+            i += 1
 
     @property
     def flag(self):
