0459671c7631b65b55cbf2e4d07cbc170059c3b1
Jason Damiani <me@jasondamiani.com>
2018-04-23 20:18:11 -0400

    Clean up variable names, move get_bits calls into PerCar

--- pyraceview/messages/MsgPitWindow.py
+++ pyraceview/messages/MsgPitWindow.py
@@ -5,19 +5,17 @@ from ..percar import PerCarPitWindowData
 
 class MsgPitWindow(object):
     def __init__(self, msg_header, byte_array):
-         _loc3_ = BitBuffer(byte_array)
-         _loc3_.set_position(7)
-         _loc4_ = _loc3_.get_bits(uint32(8))
-         _loc5_ = 0
+         bit_buffer = BitBuffer(byte_array)
+         bit_buffer.set_position(7)
+         self._num_cars = bit_buffer.get_bits(uint32(8))
+
+         i = 0
 
          self._per_car_pit_window_data = []
 
-         while _loc5_ < _loc4_:
-             _loc6_ = _loc3_.get_bits(uint32(8))
-             _loc7_ = _loc3_.get_bits(uint32(12))
-             _loc8_ = _loc3_.get_bits(uint32(12))
-             self._per_car_pit_window_data.append(PerCarPitWindowData(_loc6_,_loc7_,_loc8_))
-             _loc5_ += 1
+         while i < self._num_cars:
+             self._per_car_pit_window_data.append(PerCarPitWindowData(bit_buffer))
+             i += 1
 
     @property
     def per_car_pit_window_data(self):
--- pyraceview/percar/PerCarPitWindowData.py
+++ pyraceview/percar/PerCarPitWindowData.py
@@ -1,13 +1,16 @@
+from numpy import uint32
+
+
 class PerCarPitWindowData(object):
 
-    def __init__(self, param1:int, param2:int, param3:int):
-        self._id = param1
-        self._low = param2
-        self._high = param3
+    def __init__(self, bit_buffer):
+        self._car_id = bit_buffer.get_bits(uint32(8))
+        self._low = bit_buffer.get_bits(uint32(12))
+        self._high = bit_buffer.get_bits(uint32(12))
 
     @property
-    def id(self):
-        return self._id
+    def car_id(self):
+        return self._car_id
 
     @property
     def low(self):
