82df5698ebf278ec7ab1e74c72169fb1a3d638fb
Jason Damiani <me@jasondamiani.com>
2018-04-23 20:21:35 -0400

    Add track_name property and clean up variable names

--- pyraceview/messages/MsgTrackConfig.py
+++ pyraceview/messages/MsgTrackConfig.py
@@ -9,11 +9,11 @@ class MsgTrackConfig(object):
     TRACKCONFIG_BITS_TRACK_NAME = uint32(64)
 
     def __init__(self, msg_header, byte_array):
-        _loc3_ = BitBuffer(byte_array)
-        _loc3_.set_position(7)
-        self._local_origin_x = _loc3_.get_bits(self.TRACKCONFIG_BITS_ORIGIN_X)
-        self._local_origin_y = _loc3_.get_bits(self.TRACKCONFIG_BITS_ORIGIN_Y)
-        self._local_origin_z = _loc3_.get_bits(self.TRACKCONFIG_BITS_ORIGIN_Z)
+        bit_buffer = BitBuffer(byte_array)
+        bit_buffer.set_position(7)
+        self._local_origin_x = bit_buffer.get_bits(self.TRACKCONFIG_BITS_ORIGIN_X)
+        self._local_origin_y = bit_buffer.get_bits(self.TRACKCONFIG_BITS_ORIGIN_Y)
+        self._local_origin_z = bit_buffer.get_bits(self.TRACKCONFIG_BITS_ORIGIN_Z)
         self._track_name = byte_array.read_utf_bytes(self.TRACKCONFIG_BITS_TRACK_NAME // uint32(8))
 
     @property
@@ -27,3 +27,7 @@ class MsgTrackConfig(object):
     @property
     def local_origin_z(self):
         return self._local_origin_z
+
+    @property
+    def track_name(self):
+        return self._track_name
