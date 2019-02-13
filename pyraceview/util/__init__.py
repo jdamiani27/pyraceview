8292cc8638312f70e28f5acf3c2902d618a6b254
Jason Damiani <me@jasondamiani.com>
2019-02-12 23:43:43 -0500

    Add util function to return the epsg coordinate system used for each track

--- pyraceview/util/__init__.py
+++ pyraceview/util/__init__.py
@@ -2,3 +2,12 @@ from .BitBuffer import BitBuffer
 from .ByteArray import ByteArray
 from .Vector3D import Vector3D
 from .Endian import Endian
+
+
+_coordinate_mapping = {
+    'daytona': 'epsg:2236',
+}
+
+
+def get_coord_system(track_name):
+    return _coordinate_mapping[track_name]
