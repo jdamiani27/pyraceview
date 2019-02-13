0b9a93c139922553f5a54c6e550280921364ce85
Jason Damiani <me@jasondamiani.com>
2019-02-12 23:44:49 -0500

    Add util function to convert internal raceview car_id's to proper car_num strings

--- pyraceview/util/__init__.py
+++ pyraceview/util/__init__.py
@@ -11,3 +11,10 @@ _coordinate_mapping = {
 
 def get_coord_system(track_name):
     return _coordinate_mapping[track_name]
+
+
+def id_to_num(car_id):
+    if car_id % 2 == 1:
+        return "0" + str(int((car_id - 1) / 2))
+    else:
+        return str(int(car_id / 2))
