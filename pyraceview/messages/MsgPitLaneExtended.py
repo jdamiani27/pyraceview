0393f7e5edbb22f263c5daff3b492cbc9bcebcf1
Jason Damiani <me@jasondamiani.com>
2019-06-28 22:48:42 -0400

    Simplify MsgPitLaneExtended to expect only one car

--- pyraceview/messages/MsgPitLaneExtended.py
+++ pyraceview/messages/MsgPitLaneExtended.py
@@ -5,20 +5,24 @@ from ..percar import PerCarPitStopExtendedData
 class MsgPitLaneExtended(object):
 
     def __init__(self, msg_bytes):
-        self._pits = [] # CarEntry
         byte_array = ByteArray(msg_bytes)
         endian = byte_array.endian
         byte_array.position = 7
 
         byte_array.endian = Endian.LITTLE_ENDIAN if (endian == Endian.BIG_ENDIAN) else Endian.BIG_ENDIAN
-        self._vitc_time = byte_array.read_unsigned_int()
-        self._num_cars = byte_array.read_unsigned_byte()
+        self._vitc_time = int(byte_array.read_unsigned_int())
+        num_cars = int(byte_array.read_unsigned_byte())
 
-        i = 0
+        if num_cars > 1:
+            raise ValueError(f'Expected 1 car entry, received {num_cars}')
 
-        while i < self._num_cars:
-            self._pits.append(CarEntry(byte_array))
-            i += 1
+        self._car_id = int(byte_array.read_unsigned_byte())
+        num_pits = int(byte_array.read_byte())
+
+        if num_pits > 1:
+            raise ValueError(f'Expected 1 pit entry, received {num_pits}')
+
+        self._pit_stop = PerCarPitStopExtendedData(byte_array)
 
         byte_array.endian = endian
 
@@ -27,33 +31,9 @@ class MsgPitLaneExtended(object):
         return self._vitc_time
 
     @property
-    def num_cars(self):
-        return self._num_cars
-
-    def get_car_id(self, param1):
-        return self._pits[param1].car_id
-
-    def get_car(self, param1):
-        return self._pits[param1].pits
-
-
-class CarEntry(object):
-
-    def __init__(self, byte_array):
-        self._pits = [] # PerCarPitStopExtendedData
-        self._car_id = byte_array.read_unsigned_byte()
-        self._num_cars = byte_array.read_byte()
-
-        i = 0
-
-        while i < self._num_cars:
-            self._pits.append(PerCarPitStopExtendedData(byte_array))
-            i += 1
-
-    @property
     def car_id(self):
         return self._car_id
 
     @property
-    def pits(self):
-        return self._pits
+    def pit_stop(self):
+        return self._pit_stop
