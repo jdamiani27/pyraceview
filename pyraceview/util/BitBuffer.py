979cdc401f44efa78b89b4bc85c28e23ba520458
Jason Damiani <me@jasondamiani.com>
2019-07-09 00:18:57 -0400

    Some more BitBuffer cleansing

--- pyraceview/util/BitBuffer.py
+++ pyraceview/util/BitBuffer.py
@@ -22,30 +22,27 @@ class BitBuffer(object):
 
     def get_bits(self, size):
         bits_remaining = size
-        unpacked_value = 0
+        unpacked_value = uint32(0)
 
-        # If the bit position is not 0, then we need to extract bits from the current
+        # If the bit position is not 0, we need to extract bits from the current
         # partially unpacked byte
         if self.bit_position != 0:
             extract_size = uint32(min(8 - self.bit_position, bits_remaining))
-            extracted_bits = uint32(self.extract_bits_from_byte(self.current_byte, self.bit_position, extract_size))
-            unpacked_value = uint32(unpacked_value | extracted_bits)
-            self.bit_position = self.bit_position + extract_size
-            self.bit_position = self.bit_position % 8
-            bits_remaining = bits_remaining - extract_size
+            unpacked_value = uint32(self.extract_bits_from_byte(self.current_byte, self.bit_position, extract_size))
+            self.bit_position = (self.bit_position + extract_size) % 8
+            bits_remaining -= extract_size
 
+        # While there are more than 8 bits left to read, continue unpacking whole bytes
         while bits_remaining >= 8:
-            self.current_byte = 255 & self.buffer.read_unsigned_byte()
-
+            self.current_byte = self.buffer.read_unsigned_byte()
             unpacked_value = uint32(unpacked_value << 8 | self.current_byte)
-            bits_remaining = bits_remaining - 8
+            bits_remaining -= 8
 
+        # If there are more bits left to read, partially unpack the next byte
         if bits_remaining > 0:
-            self.current_byte = 255 & self.buffer.read_unsigned_byte()
-
+            self.current_byte = self.buffer.read_unsigned_byte()
             extracted_bits = uint32(self.extract_bits_from_byte(self.current_byte, 0, bits_remaining))
             unpacked_value = uint32(unpacked_value << bits_remaining | extracted_bits)
-            self.bit_position = self.bit_position + bits_remaining
-            bits_remaining = 0
+            self.bit_position += bits_remaining
 
         return unpacked_value
