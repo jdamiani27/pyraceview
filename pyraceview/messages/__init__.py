d1a59ae9336842df536ac8812a268b07c27fbc98
Jason Damiani <me@jasondamiani.com>
2019-09-07 15:03:50 -0400

    Add MsgFactory class

--- pyraceview/messages/__init__.py
+++ pyraceview/messages/__init__.py
@@ -1,3 +1,4 @@
+from ..util import ByteBuffer
 from .MsgCarPosition import MsgCarPosition
 from .MsgCarStats import MsgCarStats
 from .MsgCupInfo import MsgCupInfo
@@ -9,3 +10,47 @@ from .MsgPitWindow import MsgPitWindow
 from .MsgRaceStatus import MsgRaceStatus
 from .MsgTrackConfig import MsgTrackConfig
 from .MsgVitcToLap import MsgVitcToLap
+
+
+_parsers = {
+    "a": MsgCarStats,
+    "b": MsgPitLaneExtended,
+    "d": MsgPitLaneExtended,
+    "C": MsgCupInfo,
+    "F": MsgPitWindow,
+    "l": MsgLapInfo,
+    "O": MsgTrackConfig,
+    "P": MsgPitLaneEvent,
+    "s": MsgRaceStatus,
+    "V": MsgVitcToLap,
+    "W": MsgCarPosition,
+}
+
+
+class MsgFactory(object):
+
+    HEADER_SIZE = 7
+
+    def __init__(self, source=b""):
+        self._buffer = ByteBuffer(source)
+
+    def push_data(self, b):
+        self._buffer.write(b)
+
+    def has_message(self):
+        available = self._buffer.size()
+        if available >= HEADER_SIZE:
+            header = MsgHeader(self._buffer.peek(HEADER_SIZE))
+            if header.size + HEADER_SIZE <= available:
+                return True
+
+        return False
+
+    def get_message(self):
+        if self.has_message():
+            header = MsgHeader(self._buffer.peek(HEADER_SIZE))
+            message_parser = _parsers[header.byte_type]
+            msg_bytes = self.buffer.read(header.size + HEADER_SIZE)
+            return message_parser(msg_bytes)
+
+        return None
