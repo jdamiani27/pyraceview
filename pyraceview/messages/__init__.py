e7191fa57faa1584ee268c79998d9b43f5636c14
Jason Damiani <me@jasondamiani.com>
2019-09-08 17:19:56 -0400

    Fix typo on buffer

--- pyraceview/messages/__init__.py
+++ pyraceview/messages/__init__.py
@@ -50,7 +50,7 @@ class MsgFactory(object):
         if self.has_message():
             header = MsgHeader(self._buffer.peek(HEADER_SIZE))
             message_parser = _parsers[header.byte_type]
-            msg_bytes = self.buffer.read(header.size + HEADER_SIZE)
+            msg_bytes = self._buffer.read(header.size + HEADER_SIZE)
             return message_parser(msg_bytes)
 
         return None
