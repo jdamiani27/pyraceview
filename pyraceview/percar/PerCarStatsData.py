64f743ebeb09d36e58012baafef5026099784187
Jason Damiani <me@jasondamiani.com>
2019-09-21 00:31:17 -0400

    Rename param

--- pyraceview/percar/PerCarStatsData.py
+++ pyraceview/percar/PerCarStatsData.py
@@ -10,13 +10,13 @@ RESERVED = uint32(2)
 
 
 class PerCarStatsData(object):
-    def __init__(self, param1):
-        self._id = param1.get_bits(BITS_CAR_NUM)
-        self._top_speed = param1.get_bits(BITS_TOP_SPEED) / 100
-        self._fastest_time = param1.get_bits(BITS_FASTEST_TIME)
-        self._average_lap = param1.get_bits(BITS_AVERAGE_LAP)
-        self._average_speed = param1.get_bits(BITS_AVERAGE_SPEED) / 100
-        param1.get_bits(RESERVED)
+    def __init__(self, bit_buffer):
+        self._id = bit_buffer.get_bits(BITS_CAR_NUM)
+        self._top_speed = bit_buffer.get_bits(BITS_TOP_SPEED) / 100
+        self._fastest_time = bit_buffer.get_bits(BITS_FASTEST_TIME)
+        self._average_lap = bit_buffer.get_bits(BITS_AVERAGE_LAP)
+        self._average_speed = bit_buffer.get_bits(BITS_AVERAGE_SPEED) / 100
+        bit_buffer.get_bits(RESERVED)
 
     @property
     def car_id(self):
