d3158a7272f090083dff4695e45b623e8f5577d0
Jason Damiani <me@jasondamiani.com>
2019-09-25 23:41:53 -0400

    Test PerCarPointsData

---
+++ tests/test_PerCarPointsData.py
@@ -0,0 +1,26 @@
+import pytest
+from pyraceview.messages import MsgCupInfo
+
+
+raw = (
+    b"\xab\xcd\x00\x00\x00tCd&\x01\x00\x00\x02?\x18\x04@P"
+    b"\x06\x0f\xf0\x08@\xf8\x0c?X\x10\x0cH\x12@H\x14?\x88"
+    b'\x16@@\x18?\xb8\x1a\x0ep\x1c>\xe0\x1e\x00\x00"\x0f'
+    b"\xa0$@x&A\x10(>\xb8*\x12\xb0,@\xd80?\xc06\x028@\t"
+    b"\x98D\n\x88H\x08\x98J\x11\xe8L\t`R\x15\x00T?\xe0V"
+    b"\x0bh^\x0b``\x13\xd8f\x00Ph\x00\x00j\x00\x00\x9a\x02"
+    b"\xd0\xb0?\xa8\xbe\x0f\xb0"
+)
+
+
+@pytest.fixture
+def car():
+    return MsgCupInfo(raw).car_data[-1]
+
+
+def test_car_id(car):
+    assert car.car_id == 190
+
+
+def test_points(car):
+    assert car.points == 502
