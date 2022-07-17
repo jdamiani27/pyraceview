7463844d0bcfba8de02eb914a2e26f38865ae46f
Jason Damiani <me@jasondamiani.com>
2022-07-17 13:55:30 -0400

    Remove commented numpy code

--- pyraceview/util/ByteArray.py
+++ pyraceview/util/ByteArray.py
@@ -1,3 +1,4 @@
+import ctypes
 import struct
 from .Endian import Endian
 
@@ -10,29 +11,23 @@ class ByteArray(object):
 
     def read_unsigned_byte(self):
         return self.read("B")
-        # return np.uint32(self.read(np.uint8))
 
     def read_byte(self):
         return self.read("b")
-        # return np.int32(self.read(np.int8))
 
     def read_unsigned_short(self):
         return self.read(self.endian.value + "H")
-        # return self.read(np.dtype(np.uint16).newbyteorder(self._endian.value))
 
     def read_short(self):
         return self.read(self.endian.value + "h")
-        # return self.read(np.dtype(np.int16).newbyteorder(self._endian.value))
 
     def read_unsigned_int(self):
         return self.read(self.endian.value + "I")
-        # return self.read(np.dtype(np.uint32).newbyteorder(self._endian.value))
 
     def read_utf_bytes(self, length):
         return ctypes.create_string_buffer(self.read(str(length) + "s")).decode("utf-8")
 
     def read(self, format_char):
-        # val = np.frombuffer(self.buffer, dtype=dtype, count=1)[0]
         val = struct.unpack_from(format_char, self.buffer)[0]
         self.position += struct.calcsize(format_char)
 
